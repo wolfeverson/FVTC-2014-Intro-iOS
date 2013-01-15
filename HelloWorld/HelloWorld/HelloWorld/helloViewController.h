ee90278c8ac67f6274e30938ea73096be696de24
Jared Everson <wolfeversongithub@gmail.com>
2013-01-14 19:19:43 -0600

    Initial Project Creation - HelloWorld

---
+++ HelloWorld/HelloWorld/HelloWorld/helloViewController.h
@@ -0,0 +1,13 @@
+//
+//  helloViewController.h
+//  HelloWorld
+//
+//  Created by student on 1/14/13.
+//  Copyright (c) 2013 wolfeverson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface helloViewController : UIViewController
+
+@end
---
+++ HelloWorld/HelloWorld/HelloWorld/helloViewController.m
@@ -0,0 +1,29 @@
+//
+//  helloViewController.m
+//  HelloWorld
+//
+//  Created by student on 1/14/13.
+//  Copyright (c) 2013 wolfeverson. All rights reserved.
+//
+
+#import "helloViewController.h"
+
+@interface helloViewController ()
+
+@end
+
+@implementation helloViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
