8b1df11a02e267f61f58c86dc3df97bc050fbaab
Jared Everson <wolfeversongithub@gmail.com>
2013-01-14 20:25:54 -0600

    Inital Project - Practice Assignment
    Objective C joke

---
+++ HelloWorld/ObjectiveJoke/ObjectiveJoke/ObjectiveJoke/jokeViewController.m
@@ -0,0 +1,30 @@
+//
+//  jokeViewController.m
+//  ObjectiveJoke
+//
+//  Created by student on 1/14/13.
+//  Copyright (c) 2013 wolfeverson. All rights reserved.
+//
+
+#import "jokeViewController.h"
+
+@interface jokeViewController ()
+
+@end
+
+@implementation jokeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [messageLabel setText:@"Insert lame Objective-C joke here...."];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
